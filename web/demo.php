51b53513e32389cd352de79e79d0882a84527998
caokaiyan <caokaiyan@carkusoft.com>
2016-12-12 18:16:35 +0800

    add demo.php

--- web/demo.php
+++ web/demo.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>菜鸟教程(runoob.com)</title>
-
+<meta name="viewport" content="width=device-width,user-scalable=0"/>
 <style>
 div
 {
@@ -25,13 +25,34 @@ div
 	from {background:red;}
 	to {background:yellow;}
 }
+myHero {
+	display: block;
+	background-color: #ddd;
+	padding: 50px;
+	font-size: 30px;
+}
 </style>
 </head>
 <body>
 
 <p><b>注意:</b> 该实例在 Internet Explorer 9 及更早 IE 版本是无效的。</p>
+<a href="#">测试超链接</a>
+<div>
+</div>
 
-<div></div>
+<myHero>我的第一个新元素</myHero>
 
+<script>
+	document.createElement("myHero");
+	if(typeof(Storage)!=="undefined")
+	{
+		console.log('support');
+	    // 是的! 支持 localStorage  sessionStorage 对象!
+	    // 一些代码.....
+	} else {
+	    // 抱歉! 不支持 web 存储。
+		console.log('can not support');
+	}
+</script>
 </body>
 </html>
