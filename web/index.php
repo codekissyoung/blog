ddf391a7658d1afb442d541b5edc9777c56e4553
codekissyoung <cky951010@163.com>
2016-12-22 02:21:05 +0800

    add

--- web/index.php
+++ web/index.php
@@ -9,13 +9,14 @@ use HyperDown\Paser;
 <html>
 	<head>
 		<title>Codekissyoung</title>
-		<link href="/css/common.css" rel="stylesheet"/>
-		<!-- <link href="http://github.com/yrgoldteeth/darkdowncss/raw/master/darkdown.css" rel="stylesheet"></link> -->
+		<link href="/css/common.css?time=<?=time();?>" rel="stylesheet"/>
 		<link href="/highlight.js/src/styles/github.css" rel="stylesheet">
 		<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
 	</head>
 	<body>
-	<?=file_tree_print(file_tree(MD_ROOT))?>
+	<nav class="main_category">
+		<?=file_tree_print(file_tree(MD_ROOT))?>
+	</nav>
 	<?php if(isset($_GET['a'])):?>
 		<?php
 			$article = $_GET['a'];
@@ -24,11 +25,26 @@ use HyperDown\Paser;
 			$parser = new HyperDown\Parser;
 			$html = $parser -> makeHtml($content);
 		?>
-		<p>当前文章:<?=$article?></p>
-		<?=$html;?>
+		<div id="article">
+			<!-- <p>当前文章:<?=$article?></p> -->
+			<div>
+				<?=$html;?>
+			</div>
+		</div>
 	<?php endif;?>
 	<script>
 		hljs.initHighlightingOnLoad();
+		var as = document.getElementsByTagName('a');
+		// console.log(as);
+		console.log();
+		for(var x in as ){
+			if(location.href == as[x].href){
+				console.log(as[x].href);
+				as[x].className = "active";
+			}
+
+		}
+
 	</script>
 	</body>
 </html>
