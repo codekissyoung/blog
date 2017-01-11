24212c1a61e47cb2752a6d6e3d85f736751787e2
caokaiyan <cky951010@163.com>
2017-01-12 02:16:46 +0800

    add files

--- function/common.php
+++ function/common.php
@@ -1,15 +1,16 @@
 <?php
 // $path 路径 返回目录树数组
 function file_tree($path){
-	$tree = scandir($path,1);
+	$tree = scandir($path);
 	foreach($tree as $key => $leaf){
 		if($leaf == "." || $leaf == ".."){
 			unset($tree[$key]);
 		}elseif(is_dir($path.'/'.$leaf)){
 			unset($tree[$key]);
-			$tree[$leaf] = file_tree($path.'/'.$leaf);
+			$tree[$leaf] = array_reverse(file_tree($path.'/'.$leaf));
 		}
 	}
+	error_log(var_export($tree,true));
 	return $tree;
 }
 
@@ -18,15 +19,12 @@ function file_tree_print($tree,$title_i = false,$path = false){
 	$i = 1; // 计数
 	$html = "<ul>"; // 要生成的html代码
 	foreach($tree as $key => $leaf){
-		if(is_string($leaf)){
-			if(!preg_match("/md$/",$leaf))
-			{
-				continue;
-			}
-			$html .= $title_i ? "<li><a href='/?a=$path/$leaf'>$title_i.$i $leaf</a></li>":"<li><a href='/?a=$leaf'>$i $leaf</a></li>";
-		}else{
+		if(!is_string($leaf)){
 			$html .= $title_i ? "<li><h2>$title_i.$i $key</h2>".file_tree_print($leaf,$title_i.'.'.$i,$path."/".$key)."</li>":
 			"<li><h2>$i $key</h2>".file_tree_print($leaf,$i,$key)."</li>";
+		}else{
+			if(!preg_match("/md$/",$leaf)) continue;  // 跳过不是md结尾的文件
+			$html .= $title_i ? "<li><a href='/?a=$path/$leaf'><span class='head-tag'>$title_i.$i</span> $leaf</a></li>":"<li><a href='/?a=$leaf'>$i $leaf</a></li>";
 		}
 		$i++;
 	}
