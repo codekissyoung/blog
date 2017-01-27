f81d6d2245e6c29c2765dbfba883168e1c17d422
caokaiyan <cky951010@163.com>
2017-01-27 22:57:51 +0800

    add files

--- web/js.php
+++ web/js.php
@@ -6,6 +6,7 @@
 <body>
 <script>
 'use strict';
+
 var m = new Map();
 var s = new Set([2,3,4,5,67,75,5,'new']);
 
@@ -14,6 +15,7 @@ m.set('name','codekissyoung');
 
 console.log(m.has('name2'));
 console.log(m.get('age'));
+
 console.log(m);
 console.log(s);
 
@@ -24,6 +26,12 @@ for(var x of s){
 for(var x of m){
 	console.log(x[0] + " : " +  x[1]);
 }
+
+m.forEach(function(value,key,map){
+	console.log(value);
+	console.log(key);
+});
+
 </script>
 </body>
 </html>
