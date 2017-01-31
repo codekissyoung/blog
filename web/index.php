f3e49d844584edaa1a628a5bfd7c03cc3cb3202b
caokaiyan <cky951010@163.com>
2017-01-31 21:22:18 +0800

    add files

--- web/index.php
+++ web/index.php
@@ -16,7 +16,10 @@ if(isset($a)){
 	$content = file_get_contents(MD_ROOT.'/'.$article);
 	$parser = new HyperDown\Parser;
 	$html = $parser -> makeHtml($content);
-
+}else{
+	$content = file_get_contents(MD_ROOT.'/3500.txt');
+	$parser  = new HyperDown\Parser;
+	$html    = $parser -> makeHtml($content);
 }
 
 // 使用有字库字体
@@ -25,43 +28,73 @@ $you_yuan = '043c1fde632445c2aaeb57e8631c65b4'; // 幼圆字体
 $shi_yuan_hei_ti = '7a5e764d9e6948abb90bd31b847c9ec2'; //思源黑体Light
 $microsoft_yahei = '5ab7d6244ba44de8b48439e88019a817'; // 微软雅黑
 $param = [
-	"accessKey"=>$shi_yuan_hei_ti,
-	"content"=> $category . $html,
-	"tag"=>"*"];
+	"accessKey" => $shi_yuan_hei_ti,
+	// "accessKey" => $microsoft_yahei,
+	"content" => $category . $html,
+	"tag" => "*"];
 $response = $youzikuClient -> GetFontFace($param);
+
+if(isset($a)){
+	/*
+	echo '<style>';
+	echo $response -> FontFace;
+	echo '</style>';
+	*/
+	echo '<div>'.$html.'</div>';
+	exit;
+}
+
 ?>
 <!DOCTYPE HTML>
 <html>
 	<head>
 		<title><?=isset($article) ? $article.' ' :'';?>Codekissyoung Blog</title>
-        <link href="//cdn.bootcss.com/normalize/5.0.0/normalize.min.css" rel="stylesheet">
+		<link href="//cdn.bootcss.com/normalize/5.0.0/normalize.min.css" rel="stylesheet">
 		<link href="/css/common.css?time=<?=time();?>" rel="stylesheet"/>
 		<link href="/highlight.js/src/styles/github.css" rel="stylesheet">
-        <script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
+		<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
-        <script src="/js/common.js"></script>
+		<script src="/js/common.js"></script>
 		<style>
 		 <?=$response -> FontFace;?>
 		</style>
 	</head>
 	<body>
+	<?php if(!isset($a)):?>
 	<nav id="main_category">
 		<h1 class="main-title">Codekissyoung Blog</h1>
 		<?=$category?>
 	</nav>
+	<?php endif; ?>
 	<div id="article">
 		<div>
 			<?=$html;?>
 		</div>
 	</div>
 	<script>
+		// 代码高亮
 		hljs.initHighlightingOnLoad();
-		var as = document.getElementsByTagName('a');
-		for(var x in as ){
-			if(location.href == as[x].href){
-				as[x].className = "active";
-			}
-		}
+		
+		// 异步加载文章
+		$('#main_category a').on('click',function(){
+			$("#main_category a").removeClass('active');
+			$(this).addClass('active');
+			var href = $(this).attr('href');
+			$.ajax({
+				url:href,
+				type:'GET',
+				data:{},
+				dataType:'text',
+				timeout:5000,
+				success:function(data){
+					$("#article").empty().append($(data)).find('pre code').each(function(i,block){
+						hljs.highlightBlock(block);
+					});
+				}
+			});
+			return false; // 阻止冒泡 阻止事件
+		});
 	</script>
 	</body>
 </html>
+
