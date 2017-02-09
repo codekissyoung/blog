b7337fcd2771d0097e8f28528db41f2054175ef8
caokaiyan <caokaiyan@carkusoft.com>
2017-02-09 14:27:49 +0800

    add files

---
+++ web/demo.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>css</title>
+        <style>
+            *{
+                margin: 0px;
+                padding: 0px;
+                /*outline: 1px solid red;*/
+            }
+            .d1{
+                margin:40px 0px;
+                background-color:#eeffff;
+                padding:10px 0px;
+
+            }
+            .d2{
+                margin: 20px 0px;
+                height: 50px;
+                background-color: #eeeeee;
+            }
+            .d3{
+                margin:20px 0px;
+                padding:10px 0px;
+                background-color: pink;
+                height: 0;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="height:10px;background-color:black;">
+        </div>
+        <div class="d1">
+            <div class="d2">
+            </div>
+        </div>
+
+        <div class="d3">
+        </div>
+
+    </body>
+</html>
