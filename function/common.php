ebe8429380176e4bc90a737fe3614ac6e505739f
caokaiyan <cky951010@163.com>
2017-05-02 19:19:36 +0800

    改变目录顺序

--- function/common.php
+++ function/common.php
@@ -2,6 +2,7 @@
 // $path 路径 返回目录树数组
 function file_tree($path){
 	$tree = scandir($path);
+	sort($tree,SORT_NUMERIC); // 按照数值大小排序数组，主要考虑到看书的笔记，目录方便记录
 	foreach($tree as $key => $leaf){
 		if($leaf == "." || $leaf == ".."){
 			unset($tree[$key]);
