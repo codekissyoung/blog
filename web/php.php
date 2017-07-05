d0db5348e2eb3410c3dde49facadcadcc801eb51
codekissyoung <cky951010@163.com>
2017-07-05 21:23:08 +0800

    update blog

--- web/php.php
+++ web/php.php
@@ -1,13 +1,24 @@
 <?php
+namespace A;
 class Father{
-    static function create(){
-        return new self();
+    public function show(){
+        echo static::data();
     }
 }
 class Son extends Father{
+    public $pri = 'pri';
+    public function data(){
+        return "Son data {$this -> pri}\n";
+    }
 }
 class Girl extends Father{
+    public function data(){
+        return "Girl data\n";
+    }
 }
 
-var_dump(Son::create());
-var_dump(Girl::create());
+$s = new Son();
+$s -> show(); // Son data pri , 因为执行者是 Son ,所以 static 指向的是Son
+
+
+echo Son::class;
