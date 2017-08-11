4ed6e9ca51e299e9365d86c900337f558200f147
caokaiyan <cky951010@163.com>
2017-08-11 23:35:19 +0800

    添加百度统计代码

--- codekissyoung/resources/views/welcome.blade.php
+++ codekissyoung/resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="baidu-site-verification" content="ZhYNGyoyvk" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
         <title>Code Kiss Young</title>
 
