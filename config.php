aea6022d6388d05631e0b1707a262803ccd30a30
caokaiyan <cky951010@163.com>
2018-01-31 17:17:07 +0800

    update

--- config.php
+++ config.php
@@ -2,8 +2,7 @@
 define("APP_ROOT",__DIR__);
 define("MD_ROOT",APP_ROOT."/web/md");
 
-// "accessKey" => $shi_yuan_hei_ti,
-$you_yuan = '043c1fde632445c2aaeb57e8631c65b4'; // 幼圆字体
+$you_yuan        = '043c1fde632445c2aaeb57e8631c65b4'; // 幼圆字体
 $shi_yuan_hei_ti = '7a5e764d9e6948abb90bd31b847c9ec2'; //思源黑体Light
 $microsoft_yahei = '5ab7d6244ba44de8b48439e88019a817'; // 微软雅黑
 
