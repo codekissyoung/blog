91a682f84954ea9b03f156a830b8f68375272c39
caokaiyan <cky951010@163.com>
2018-02-27 18:29:11 +0800

    udpate

--- web/index.php
+++ web/index.php
@@ -19,15 +19,15 @@ else
 // 加载文章内容
 if( is_file( $article  ) )
 {
-    $content = file_get_contents($article);
+    $content = file_get_contents( $article );
 }
 else
 {
     $content = "文章不存在"; // 404
 }
 $parser = new HyperDown\Parser();
-$html   = $parser -> makeHtml($content);
 $title  = trim(join('-',explode("/",$ri))."-CodeKissYoung Blog",'-');
+$html   = $parser -> makeHtml( $content );
 
 // 搜索全文
 if( $search_key )
@@ -56,7 +56,7 @@ if( isset($_GET['ajax']) )
 }
 else
 {
-    $category = file_tree_print( file_tree(MD_ROOT) , $ri );
+    $category = file_tree_print( file_tree( MD_ROOT ) , $ri );
     include_once 'view/index.php';
 }
 
