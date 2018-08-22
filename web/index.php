d7f4f0f2f6a0c91f96b6896b32a29173aeba3394
caokaiyan <cky951010@163.com>
2018-08-23 01:58:01 +0800

    update

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 include_once '../config.php';
-
 $host         = $_SERVER["HTTP_HOST"];
 
 $protocol = 'http://';
@@ -18,11 +17,10 @@ else
 
 $title  = trim(join('-',explode("/",$current_article))."-CodeKissYoung Blog",'-');
 
-
 // 加载文章内容
-$parser = new HyperDown\Parser();
+$parser  = new HyperDown\Parser();
 $content = is_file( $article  ) ? file_get_contents( $article ) : "文章不存在";
-$html   = $parser -> makeHtml( $content );
+$html    = $parser -> makeHtml( $content );
 
 // 搜索全文
 $search_key = isset($_GET['search_key']) ? $_GET['search_key'] : '';
@@ -34,12 +32,12 @@ if( $search_key )
     {
         foreach( $ret_arr as $key => $value )
         {
-            $ret                    = preg_match("/\.\/.*md:/", $value, $matchs );
-            $href                   = substr($matchs[0],5,-4);
-            $search_article_tag     = "<a href='$protocol$host/$href'>$href</a>&nbsp;";
-            $value                  = htmlentities(str_replace( $matchs[0], '', $value ));
-            $value                  = str_replace( $search_key ,'<span class=search_key>'.$search_key.'</span>', $value );
-            $html                   = $html."<li class=search-list> $search_article_tag $value </li>";
+            $ret                = preg_match("/\.\/.*md:/", $value, $matchs );
+            $href               = substr($matchs[0],5,-4);
+            $search_article_tag = "<a href='$protocol$host/$href'>$href</a>&nbsp;";
+            $value              = htmlentities(str_replace( $matchs[0], '', $value ));
+            $value              = str_replace( $search_key ,'<span class=search_key>'.$search_key.'</span>', $value );
+            $html               = $html."<li class=search-list> $search_article_tag $value </li>";
         }
     }
     $html .= "</ul>";
